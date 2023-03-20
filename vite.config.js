cfa21aab07795f5c572950bfbf0276894fbee1f7
marceelp <marcelpedaa@googlemail.com>
2023-03-20 08:42:23 +0100

    add: .github

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Testing tomorrow morning if this change below worked
-  build: {
-    minify: false,
-  },
+  base: "/marceelp.github.io/",
 });
